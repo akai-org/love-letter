640bace1f4ff448d44a6046c5e02cb4e3bbe4baa
Franciszek Kaczmarek <franek.kaczmarek45gmail.com>
2023-11-16 20:55:28 +0100

    Add react-router-dom and create basic routing for BrowserPage and LobbyPage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from "react-router-dom";
 import "./index.css";
+import BrowserPage from "./Pages/BrowserPage.tsx";
+import LobbyPage from "./Pages/LobbyPage.tsx";
 
-// w tym miejscu będzie trzeba zrobić routing do różnych komponentów aplikacji
-// np. do wyszukiwarki gier, gry, do lobby, do ekranu logowania, etc.
-// najlepiej przy użyciu react-router-dom
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="/" element={<BrowserPage />} />
+      <Route path="/lobby" element={<LobbyPage />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
