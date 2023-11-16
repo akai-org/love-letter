a5b16f235794f329f895d614edffc662e8287ad9
Maciej Wojtkowiak <49617310+MaciejWojt@users.noreply.github.com>
2023-11-16 17:40:42 +0100

    Update api.py
    Added list available game rooms which has not yet started

--- backend/src/routers/api.py
+++ backend/src/routers/api.py
@@ -1,8 +1,31 @@
-from fastapi import APIRouter
+from fastapi import FastAPI
 
-router = APIRouter()
+app = FastAPI()
 
+games_data = [
+    {
+        'game_id': '1',
+        'max_players': 6,
+        'players': 5
+    },
+    {
+        'game_id': '2',
+        'max_players': 6,
+        'players': 3
+    }
+]
 
-@router.get("/")
-async def hello():
-    return {"message": "Hello world!"}
+@app.get("/api/v1/games")
+async def get_games(started: bool):
+    if started==False:
+        games = [
+            {
+                'game_id': game['game_id'],
+                'max_players': game['max_players'],
+                'players': game['players']
+            }
+            for game in games_data
+        ]
+        return {'games': games}
+    else:
+        return {'message': 'all games has already started'}
